f3e4aab6976a40e6870c6327c63e67d6a4435dc0
dcwik96 <greek96@interia.pl>
2017-10-11 20:30:36 +0200

    More input, validation

--- src/main/java/com/example/servletjspdemo/web/DataServlet.java
+++ src/main/java/com/example/servletjspdemo/web/DataServlet.java
@@ -2,6 +2,9 @@ package com.example.servletjspdemo.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,31 +25,64 @@ public class DataServlet extends HttpServlet {
 		response.setContentType("text/html");
 		
 		PrintWriter out = response.getWriter();
-		
-		String selectedHobby = "";
-		for (String hobby : request.getParameterValues("hobby")) {
-			selectedHobby += hobby + " ";
+
+		String firstName = "";
+		if(request.getParameter("firstName").matches("[A-Z]?[a-z]+$")){
+			firstName = request.getParameter("firstName");
+		} else {
+			firstName = "ERROR";
 		}
-		
-		String selectedNumbers = "";
-		for (String select : request.getParameterValues("select")) {
-			selectedNumbers += select + " ";
+
+		String lastName = "";
+		if(request.getParameter("lastName").matches("[A-Z]?[a-z]+$")){
+			lastName = request.getParameter("lastName");
+		} else {
+			lastName = "ERROR";
 		}
-		
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+		LocalDate localDate = LocalDate.now();
+		LocalDate birthDate = LocalDate.parse(request.getParameter("birthDate"));
 		String age = "";
-		if(request.getParameter("age").matches("^[0-9]+$")){
-			age = request.getParameter("age");
+		if ( birthDate.isBefore(localDate)){
+			Period period = Period.between(LocalDate.parse(request.getParameter("birthDate")),localDate);
+			age = String.valueOf(period.getYears());
 		} else {
 			age = "ERROR";
 		}
 
-		out.println("<html><body><h2>Your data</h2>" +
-				"<p>First name: " + request.getParameter("firstName") + "<br />" +
-				"<p>Your hobby: " + selectedHobby + "<br />" +
-				"<p>Your age: " + age + "<br />" +
-				"<p>Your numbers: " + selectedNumbers + "<br />" +
-				"</body></html>");
+		String phoneNumber = "";
+		if (request.getParameter("phoneNumber").matches("^[0-9]+$")) {
+			phoneNumber = request.getParameter("phoneNumber");
+		} else {
+			phoneNumber = "ERROR";
+		}
 
+		String selectedHobby = "";
+		for (String hobby : request.getParameterValues("hobby")) {
+			selectedHobby += hobby + " ";
+		}
+
+		String selectedLanguages = "";
+		for (String language : request.getParameterValues("language")) {
+			selectedLanguages += language + " ";
+		}
+
+
+		if (request.getParameter("acc") != null) {
+			out.println("<html><body><h2>Your data</h2>" +
+					"<p>First name: " + firstName + "<br />" +
+					"<p>Last name: " + lastName + "<br />" +
+					"<p>Age: " + age + "<br />" +
+					"<p>Sex: " + request.getParameter("sex") + "<br />" +
+					"<p>Phone number: " + phoneNumber + "<br />" +
+					"<p>Your hobby: " + selectedHobby + "<br />" +
+					"<p>Your description: " + request.getParameter("info") + "<br />" +
+					"<p>Languages: " + selectedLanguages + "<br />" +
+					"</body></html>");
+		} else {
+			out.println("<html><body><h2>You did not accept to the Terms and Conditions</h2></body></html>");
+		}
 		out.close();
 	}
 
--- src/main/java/com/example/servletjspdemo/web/FormServlet.java
+++ src/main/java/com/example/servletjspdemo/web/FormServlet.java
@@ -22,24 +22,36 @@ public class FormServlet extends HttpServlet {
 		response.setContentType("text/html");
 		
 		PrintWriter out = response.getWriter();
-		out.println("<html><body><h2>Simple form servlet</h2>" +
+		out.println("<html><body><h2>Introduce yourself</h2>" +
 				"<form action='data'>" +
 				"First name: <input type='text' name='firstName' /> <br />" +
-				"<input type='checkbox' name='hobby' value='bicycle'>I like riding a bicycle<br />" +
+
+				"Last name: <input type='text' name='lastName' /> <br />" +
+
+				"Birth date: <input type='date' name='birthDate'/> <br />" +
+
+				"Phone number: <input type='text' name='phoneNumber''/> <br />" +
+
+				"Sex: <select name='sex'>" +
+				"	<option value='male'>Male</option>" +
+				"	<option value='female'>Female</option>" +
+				"</select> <br /><br />" +
+
+				"What is your hobby?<br /><input type='checkbox' name='hobby' value='bicycle'>I like riding a bicycle<br />" +
 				"<input type='checkbox' name='hobby' value='tv'>I like watching TV<br />" +
 				"<input type='checkbox' name='hobby' value='beer'>I like dringking beer<br />" +
 				"<input type='checkbox' name='hobby' value='books'>I like reading books<br /><br />" +
 				
-				"<input type='textarea' name='age' value='How old are you?'><br /><br />" +
+				"<input type='textarea' name='info' value='Tell something about you'><br /><br />" +
 				
-				"<select name='select' multiple>" +
-				"    <option value='first'>1</option>" +
-				"    <option value='second'>2</option>" +
-				"    <option value='third'>3</option>" +
-				"    <option value='fourth'>4</option>" +
-				"</select>Wybierz liczbe/liczby<br /><br />" +
+				"Choose programming languages which you know:<br /><select name='language' multiple>" +
+				"    <option value='java'>Java</option>" +
+				"    <option value='c'>C</option>" +
+				"    <option value='c#'>C#</option>" +
+				"    <option value='python'>Python</option>" +
+				"</select><br /><br />" +
 				
-				"<input type='radio' name='acc' value='radio'>Akceptuje warunki umowy<br />" +
+				"<input type='radio' name='acc' value='radio'>I agree to the Terms and Conditions<br />" +
 				
 				"<input type='submit' value=' OK ' />" +
 				"</form>" +
